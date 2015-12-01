cdca7dd68b65ff649727087764de29cbdf919a00
Nick Gill <GILL@Gills-MacBook-Pro.local>
2015-12-01 15:20:49 -0600

    Changed everything

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var memes = require('../public/static/memes.json');
-var comments = require('../public/static/comments.json')
+var memes = require('../data/memes.json');
+var comments = require('../data/comments.json')
 var fs = require('fs');
 var path = require('path');
 
@@ -9,25 +9,6 @@ var path = require('path');
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Memes', data: memes });
 });
-
-// router.get('/memes/:id', function(req, res, next){
-//
-//   var memeID = req.params.id;
-//
-//   var currentMeme;
-//     for(i = 0; i < memes.length; i++) {
-//       if(memes[i].id == memeID) {
-//         currentMeme = memes[i];
-//       }
-//     }
-//     var firstComment;
-//       for (i = 0; i < comments.length; i++) {
-//         if(comments[i].imageID == memeID) {
-//           firstComment = comments[i];
-//         }
-//       }
-// })
-
 
 
 
--- routes/memes.js
+++ routes/memes.js
@@ -1,20 +1,29 @@
+
 var express = require('express');
 var router = express.Router();
-var memes = require('../public/static/memes.json');
-var comments = require('../public/static/comments.json')
+var memes = require('../data/memes.json');
+var comments = require('../data/comments.json')
 var fs = require('fs');
 var path = require('path');
 
 /* GET home page. */
 router.get('/:id', function(req, res, next) {
-  var meme = [];
+ var currentMeme;
+ var currentComment;
 
-  for (i = 0; i < memes.length; i++) {
-    if(memes[i].id === req.params.id) {
-      meme.push(memes[i]);
-    }
-  }
-  res.render('index', { title: 'Meme ' + req.params.id, data: meme });
+ for (i = 0; i < memes.length; i++) {
+   if(memes[i].id === req.params.id) {
+     currentMeme = memes[i];
+   }
+ }
+
+ for (j = 0; j < comments.length; j++) {
+   if (comments[j].imageID === req.params.id) {
+       currentComment = comments[j];
+   }
+ }
+
+   res.render('meme', { meme: currentMeme, comment: currentComment });
 });
 
 module.exports = router;
